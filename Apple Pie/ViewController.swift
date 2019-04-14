3eca6410514f1ddb10c9261a4d9fb3f94c31bd21
maahd <maahdsh@gmail.com>
2019-04-14 21:01:30 +0200

    Set up UI

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandascent", "bug", "program"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLosses = 0
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
+    }
+    
+    func newRound() {
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
